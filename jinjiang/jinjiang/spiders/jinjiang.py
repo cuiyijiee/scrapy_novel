e62d2538c4a952114a0eed6cfe45d5ceaca80a90
CUJE <cyj976655@gmail.com>
2019-03-18 23:25:43 +0800

    a

--- jinjiang/jinjiang/spiders/jinjiang.py
+++ jinjiang/jinjiang/spiders/jinjiang.py
@@ -6,10 +6,17 @@ import re
 
 
 class jinjiang(Spider):
-
     name = "jinjiang"
-    start_urls = ['http://www.jjwxc.net']
-    allow_domains = ['http://www.jjwxc.net/']
+    start_urls = ['http://app-cdn.jjwxc.net']
+    allow_domains = ['http://app-cdn.jjwxc.net/']
 
     def parse(self, response):
+        for aricle_id in range(1, 5000000,5):
+            article_url = 'http://app-cdn.jjwxc.net/androidapi/novelbasicinfo?novelId=' + str(aricle_id)
+            yield Request(url=article_url,callback= self.parseArticle,meta={
+                'article_id': aricle_id
+            })
 
+
+    def parseArticle(self,response):
+        print(response.body)
