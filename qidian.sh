6f90d1c6b04772ca3d8517456d18cbcc89560164
CUJE <cyj976655@gmail.com>
2019-06-11 22:25:46 +0800

    更新脚本

--- qidian.sh
+++ qidian.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+cd /home/scrapy_novel
+
 nohup python3.7 qidian.py>log.txt 2>&1 &
--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+cd /home/scrapy_novel
+
 nohup python3.7 biqugecc.py>log.txt 2>&1 &
 nohup python3.7 zhuishubang.py>log.txt 2>&1 &
 nohup python3.7 i7wx.py>log.txt 2>&1 &
+nohup python3.7 luoqiu.py>log.txt 2>&1 &
