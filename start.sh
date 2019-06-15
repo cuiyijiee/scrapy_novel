845c4d41bb42f5baceab8007bbeaaaf37c7031ae
CUJE <cyj976655@gmail.com>
2019-06-15 17:05:57 +0800

    添加du00

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ nohup python3.7 biqugecc.py>log.txt 2>&1 &
 nohup python3.7 zhuishubang.py>log.txt 2>&1 &
 nohup python3.7 i7wx.py>log.txt 2>&1 &
 nohup python3.7 luoqiu_pc.py>log.txt 2>&1 &
+nohup python3.7 du00.py>log.txt 2>&1 &
