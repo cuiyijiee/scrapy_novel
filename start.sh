5a849118f08a06911cbacb620306a64e1fdbdb50
CUJE <cyj976655@gmail.com>
2019-06-16 00:57:25 +0800

    添加晋江文学脚本

--- start.sh
+++ start.sh
@@ -6,3 +6,4 @@ nohup python3.7 zhuishubang.py>log.txt 2>&1 &
 nohup python3.7 i7wx.py>log.txt 2>&1 &
 nohup python3.7 luoqiu_pc.py>log.txt 2>&1 &
 nohup python3.7 du00.py>log.txt 2>&1 &
+nohup python3.7 jinjiang.py>log.txt 2>&1 &
