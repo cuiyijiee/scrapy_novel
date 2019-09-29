9d5a751bcf26bac2d20a5aecaad79cf0174c509e
cuiyijie <cuiyijie@ivymobile.com>
2019-09-29 11:16:59 +0800

    a

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 cd /home/scrapy_novel
 
-nohup python3.7 biqugecc.py>log.txt 2>&1 &
-nohup python3.7 zhuishubang.py>log.txt 2>&1 &
-nohup python3.7 i7wx.py>log.txt 2>&1 &
-nohup python3.7 luoqiu_pc.py>log.txt 2>&1 &
-nohup python3.7 du00.py>log.txt 2>&1 &
-nohup python3.7 jinjiang.py>log.txt 2>&1 &
-nohup python3.7 xbiquge6.py>log.txt 2>&1 &
-nohup python3.7 bbiquge.py>log.txt 2>&1 &
+nohup python3.7 biqugecc.py>log/biqugecc.txt 2>&1 &
+nohup python3.7 zhuishubang.py>log/zhuishubang.txt 2>&1 &
+nohup python3.7 i7wx.py>log/i7wx.txt 2>&1 &
+nohup python3.7 luoqiu_pc.py>log/luoqiu.txt 2>&1 &
+nohup python3.7 du00.py>log/du00.txt 2>&1 &
+nohup python3.7 jinjiang.py>log/jinjiang.txt 2>&1 &
+nohup python3.7 xbiquge6.py>log/xbiquge6.txt 2>&1 &
+nohup python3.7 bbiquge.py>log/bbiquge.txt 2>&1 &
